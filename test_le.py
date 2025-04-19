b0ee2724ea6d7a99751df856a98f02ec273b5924
dhruvguptax <w3dhruv3@gmail.com>
2025-04-20 03:40:34 +0530

    Remove unnecessary files: pkl.py and test_le.py

--- test_le.py
+++
@@ -1,9 +0,0 @@
-from sklearn.preprocessing import LabelEncoder
-
-print("LabelEncoder works:", LabelEncoder)
-
-le = LabelEncoder()
-data = ['Male', 'Female', 'Female', 'Male']
-encoded = le.fit_transform(data)
-
-print("Encoded result:", encoded)
